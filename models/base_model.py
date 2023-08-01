581f54049601631f0efb151a4fa51b833ebd3f27
juanRCoder <guillecarck@gmail.com>
2023-07-31 18:05:34 -0700

    File: base_model

--- models/base_model.py
+++ models/base_model.py
@@ -6,7 +6,7 @@ class BaseModel:
     def __init__(self):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
-        self.updated_at = self.created
+        self.updated_at = self.created_at
 
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
@@ -16,7 +16,7 @@ class BaseModel:
 
     def to_dict(self):
         dict_copy = self.__dict__.copy()
-        dict_copy["__class__"] = self.__class__.__name
-        dict_copy["created_at"] = self.created_at.isoformat()
-        dict_copy["updated_at"] = self.updated_at.isoformat()
+        dict_copy['__class__'] = self.__class__.__name__
+        dict_copy['created_at'] = self.created_at.isoformat()
+        dict_copy['updated_at'] = self.updated_at.isoformat()
         return dict_copy
