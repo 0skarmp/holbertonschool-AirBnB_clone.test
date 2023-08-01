d9c7b98b6a6beb2b82e32e04764d9ab1a2c4c0aa
0skarmp <omoralespj@gmail.com>
2023-07-31 18:01:05 -0700

    creating a basemodel form dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -4,9 +4,19 @@ import datetime
 
 class BaseModel:
     def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = self.created
+        def __init__(self, *args, **kwargs):
+        if kwargs:
+            kwargs.pop('__class__', None)
+            for key, value in kwargs.item():
+                if key == "created_at" or key == "uptdate_at":
+                    value = datetime.datetime.strptime(value "%Y-%m-%dT%H:%M:%S.%f")
+                setattr(self, key, value)
+
+        else:
+            
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = self.created.at
 
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
@@ -16,7 +26,10 @@ class BaseModel:
 
     def to_dict(self):
         dict_copy = self.__dict__.copy()
-        dict_copy["__class__"] = self.__class__.__name
+        dict_copy["__class__"] = self.__class__.__name__
         dict_copy["created_at"] = self.created_at.isoformat()
         dict_copy["updated_at"] = self.updated_at.isoformat()
         return dict_copy
+
+    
+        
