a6d365c48ad792b0b75166aa8b2be8f5150cfadf
juanRCoder <guillecarck@gmail.com>
2023-08-01 08:39:11 -0700

    File: base_model the console

--- models/base_model.py
+++ models/base_model.py
@@ -3,18 +3,18 @@ import uuid
 import datetime
 
 class BaseModel:
-        def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         if kwargs:
             kwargs.pop('__class__', None)
-            for key, value in kwargs.item():
-                if key == "created_at" or key == "uptdate_at":
-                    value = datetime.datetime.strptime(value "%Y-%m-%dT%H:%M:%S.%f")
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 setattr(self, key, value)
 
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.now()
-            self.updated_at = self.created.at
+            self.updated_at = self.created_at
 
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
