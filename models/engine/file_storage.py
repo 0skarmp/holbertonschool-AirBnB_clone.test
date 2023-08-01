9a9a59e59084450bc0c74548728a959e5e6eaacb
0skarmp <omoralespj@gmail.com>
2023-08-01 10:56:24 -0700

    file :storage

---
+++ models/engine/file_storage.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import json
+import os
+
+class FileStorage:
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        return: FileStorage.__objects
+
+    def new(self, obj):
+        key = f"{obj.__class__.__name__.}.{obj.id}"
+        FileStorage.__objects[key] = obj
+
+    def save(self):
+        with open(FileStorage.__file_path, "w") as txt:
+            json.dumps(FileStorage.__objects, txt)
+
+    def reload(self):
+        if os.path exists(FileStorage.__file_path):
+            with open(FileStorage.__file_path, "W") as txt:
+                json.loads(txt)
