3e7177a86fe21c9116483c7f6304405fe192d400
0skarmp <omoralespj@gmail.com>
2023-08-02 17:23:43 -0700

    file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,22 +2,27 @@
 import json
 import os
 
+
 class FileStorage:
+    """this is a class filestorgare"""
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
+        """this a method all"""
         return FileStorage.__objects
 
     def new(self, obj):
+        """this a method new that generate a new isntance"""
         key = f"{obj.__class__.__name__}.{obj.id}"
         FileStorage.__objects[key] = obj
 
     def save(self):
+        """this method serealized the dictionary in JSON"""
         dic_serialized = {}
         for key, value in FileStorage.__objects.items():
             dic_serialized[key] = value.to_dict()
-            
+
         with open(FileStorage.__file_path, "w", encoding='utf-8') as file:
             json.dump(dic_serialized, file)
 
@@ -32,4 +37,4 @@ class FileStorage:
 
         if class_name == "BaseModel":
             obj = BaseModel(**value)
-            FileStorage.__object[key] = obj 
+            FileStorage.__object[key] = obj
