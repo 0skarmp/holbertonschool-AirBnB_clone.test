770ccb2d7159cfc1a17aced3bd6808fbed6034f4
juanRCoder <guillecarck@gmail.com>
2023-08-02 20:17:17 -0700

    File: file_storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,37 +4,34 @@ import os
 
 
 class FileStorage:
-    """this is a class filestorgare"""
-    __file_path = "file.json"
-    __objects = {}
+    def __init__(self):
+        """this is a class filestorgare"""
+        self.__file_path = "file.json"
+        self.__objects = {}
 
     def all(self):
         """this a method all"""
-        return FileStorage.__objects
+        return self.__objects
 
     def new(self, obj):
         """this a method new that generate a new isntance"""
         key = f"{obj.__class__.__name__}.{obj.id}"
-        FileStorage.__objects[key] = obj
+        self.__objects[key] = obj.to_dict()
 
     def save(self):
         """this method serealized the dictionary in JSON"""
         dic_serialized = {}
-        for key, value in FileStorage.__objects.items():
-            dic_serialized[key] = value.to_dict()
+        for key, value in self.__objects.items():
+            dic_serialized[key] = value
 
-        with open(FileStorage.__file_path, "w", encoding='utf-8') as file:
+        with open(self.__file_path, "w", encoding='utf-8') as file:
             json.dump(dic_serialized, file)
 
     def reload(self):
         """Deserialize the JSON file __file_path to __objects, if it exists."""
-        if os.path.exists(FileStorage.__file_path):
-            with open(FileStorage.__file_path, 'r') as f:
-                data = json.load(f)
-
-            for key, value in data.items():
-                class_name, obj_id = key.split(".")
-
-                if class_name == "BaseModel":
-                    obj = BaseModel(**value)
-                    FileStorage.__object[key] = obj
+        if os.path.exists(self.__file_path):
+            with open(self.__file_path, 'r') as f:
+                data = f.read()
+            self.__objects = json.loads(data)
+        else:
+            pass
