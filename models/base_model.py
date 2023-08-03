8097f8c203be62c39a3a5cece0bebf157a363dc3
juanRCoder <guillecarck@gmail.com>
2023-08-02 17:25:20 -0700

    File: base_model

--- models/base_model.py
+++ models/base_model.py
@@ -20,7 +20,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.now()
             self.updated_at = self.created_at
-            storage.new(selee)
+            storage.new(self)
 
     def __str__(self):
         """ __str__ special method"""
