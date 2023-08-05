72728f5676e7222254b34e88dcbad3557ed043ca
juanRCoder <guillecarck@gmail.com>
2023-08-05 13:40:13 -0700

    CMD : The Console

--- console.py
+++ console.py
@@ -4,14 +4,16 @@ this console define a "airBnB" console.
 """
 import cmd
 import sys
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
     """
-    this class HBNB define
+    this class ommand
    the classes of the console.
     """
     prompt = '(hbnb)'
+    l_class = ['BaseModel']
 
     def do_quit(self, arg):
         """  quit command to exit the program"""
@@ -26,7 +28,19 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Do nothing when an empty line is entered"""
         pass
+    
+    def do_create(self, arg):
+        """Do created new instance of the class and print ID"""
+        if not arg:
+            print("** class name missing **")
 
+        elif arg not in HBNBCommand.l_class: 
+            print("** class doesn't exist **")
+        
+        else:
+            dct = {'BaseModel': BaseModel}
+            obj = dct[arg]()
+            print(obj.id)
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
